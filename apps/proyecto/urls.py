3d6a0ec9df9328738539c749cc1c2736da3fe4a3
Huiza <mejia_huiza@hotmail.com>
2021-05-14 16:36:59 -0600

    Consulta de estudiante por porcentaje de carrera aprobado,genero y modalidad

--- apps/proyecto/urls.py
+++ apps/proyecto/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
     
     # ESTA URL ES SOLO PARA QUE FUNCIONE EL EJEMPLO, LUEGO SE BORRARA
     path('istemaGerencialAgro/consultaEstudiante/', consultaEstudiante, name="consulta_estudiante"),
+    path('sistemaGerencialAgro/porcentaje/', reportesEstudiantesPorcentajeCarrera.as_view(), name="buscar_porcentaje"),
+    path('sistemaGerencialAgro/genero/', reportesEstudiantesPorGenero.as_view(), name="buscar_genero"),
+    path('sistemaGerencialAgro/modalidad/', reportesEstudiantesPorModalidad.as_view(), name="buscar_modalidad"),
+    path('export/excel', export_estudiantes_csv, name='export_csv'),
 ]
--- apps/proyecto/views.py
+++ apps/proyecto/views.py
@@ -9,9 +9,10 @@ from django.contrib import messages
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
-
+from django.views.generic import TemplateView
+from django.http import HttpResponse
 from .models import *
-
+import xlwt
 
 #------------------------------------------------------------------------------
 
@@ -48,5 +49,56 @@ def consultaEstudiante(request):
         'proyecto/EjemploConsulta.html', context
     )
 
-
 #------------------------------------------------------------------------------
+
+class reportesEstudiantesPorcentajeCarrera(TemplateView):
+    template_name='proyecto/EstudiantesPorcentajeCarrera.html'
+    def post(self,request,*args,**kwargs):
+        porcentaje=request.POST['porcentaje']
+        estudios_universitarios=EstudioUniversitario.objects.filter(porc_carrerar_aprob=porcentaje)
+        return render(request,'proyecto/EstudiantesPorcentajeCarrera.html',{'estudios_universitarios':estudios_universitarios})
+
+class reportesEstudiantesPorGenero(TemplateView):
+    template_name='proyecto/EstudiantesPorGenero.html'
+    def post(self,request,*args,**kwargs):
+        sexo=request.POST['sexo']
+        estudiantes=Solicitud.objects.filter(carnet_estudiante__carnet_estudiante__sexo_estudiante=sexo)
+        return render(request,'proyecto/EstudiantesPorGenero.html',{'estudiantes':estudiantes})
+
+class reportesEstudiantesPorModalidad(TemplateView):
+    template_name='proyecto/EstudiantesPorModalidad.html'
+    def post(self,request,*args,**kwargs):
+        modalidad=request.POST['modalidad']
+        solicitudes=Solicitud.objects.filter(modalidad=modalidad)
+        return render(request,'proyecto/EstudiantesPorModalidad.html',{'solicitudes':solicitudes})
+
+def export_estudiantes_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="users.csv"'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Datos de estudiantes') # this will make a sheet named Users Data
+
+    # Sheet header, first row
+    row_num = 0
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = ['Carnet', 'Nombre', 'Apellido']
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style) # at 0 row 0 column 
+
+    # Sheet body, remaining rows
+    font_style = xlwt.XFStyle()
+
+    rows = Estudiante.objects.all().values_list('carnet_estudiante', 'nombre_estudiante', 'apellido_estudiante')
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, row[col_num], font_style)
+
+    wb.save(response)
+
+    return response
